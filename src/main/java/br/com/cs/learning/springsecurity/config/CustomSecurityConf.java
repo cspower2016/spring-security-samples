11135f94f95f84e0c750077f41c2b012e6085336
Augusto Silva, Carlos (ca194h) <ca194h@att.com>
2021-04-29 14:53:42 -0300

    Adds form-login authentication code sample

--- src/main/java/br/com/cs/learning/springsecurity/config/CustomSecurityConf.java
+++ src/main/java/br/com/cs/learning/springsecurity/config/CustomSecurityConf.java
@@ -15,26 +15,10 @@ public class CustomSecurityConf extends WebSecurityConfigurerAdapter {
 	// DEFINES THE AUTH METHODS THAT IS GOING TO BE USED
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// HTTP BASIC METHOD
-		http.httpBasic();
-		http.authorizeRequests().anyRequest().authenticated();
+		// FORM LOGIN METHOD
+		http.formLogin();
+		http.authorizeRequests().antMatchers("hello").authenticated().anyRequest().permitAll();
 	}
-
-	// DEFINES THE USER DETAILS MANAGER
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		// TODO Auto-generated method stub
-//
-//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//
-//		InMemoryUserDetailsManager userDetailService = new InMemoryUserDetailsManager();
-//		UserDetails userDetails = User.withUsername("admin").password(encoder.encode("123")).authorities("read")
-//				.build();
-//		userDetailService.createUser(userDetails);
-//
-//		auth.userDetailsService(userDetailService).passwordEncoder(encoder);
-//
-//	}
 
 	// USES A CUSTOM AUTH PROVIDER
 	@Override
