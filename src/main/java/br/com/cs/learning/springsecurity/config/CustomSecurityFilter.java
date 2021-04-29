ea883c5e1d3aafd8d66ad257696f9d18bf2929f5
Augusto Silva, Carlos (ca194h) <ca194h@att.com>
2021-04-29 16:10:33 -0300

    Adds bultin filter code sample

--- src/main/java/br/com/cs/learning/springsecurity/config/CustomSecurityFilter.java
+++ src/main/java/br/com/cs/learning/springsecurity/config/CustomSecurityFilter.java
@@ -8,9 +8,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
 
-public class CustomSecurityFilter implements Filter {
+
+public class CustomSecurityFilter extends GenericFilterBean {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
